6094b28b27f69570a8b7d2eeec657dad1c5e9a56
Tino Reyna <tinoreyna1984@gmail.com>
2023-03-19 11:10:30 -0500

    se aplica animaciones ligeras a las imágenes de clientes

--- components/cliente.js
+++ components/cliente.js
@@ -1,6 +1,8 @@
 export default function Cliente({ srcImg, url }) {
     return (
-        <div className="max-h-[300px] max-w-[240px]">
+        <div className="max-h-[300px] max-w-[240px]
+            lg:transition-transform lg:duration-500 lg:ease-in-out lg:hover:scale-[1.2]"
+        >
             <a href={url} target="_blank">
                 <picture>
                     <source className="p-6" srcSet={`/clientes/avif/${srcImg}.avif`} type="image/avif" />
